9ab4bdba86ed709cc2b7dc1d6f95c3f7fa977184
Seth Barberee <seth.barberee@gmail.com>
2018-05-20 21:48:05 -0500

    added tab

--- config.def.h
+++ config.def.h
@@ -11,6 +11,15 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
+
+/*   Display modes of the tab bar: never shown, always shown, shown only in */
+/*   monocle mode in presence of several windows.                           */
+/*   Modes after showtab_nmodes are disabled                                */
+enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
+static const int showtab            = showtab_auto; /* Default tab bar show mode */
+static const int toptab             = False;    /* False means bottom tab bar */
+
+
 static const char *fonts[]          = {
     "Wuncon Siji:size=15",
     "Knack Nerd Font Mono:size=15"
@@ -73,8 +82,8 @@ static const char *mutevol[] = { "ponymix", "toggle", NULL};
 static const char *playcmd[] = { "playerctl", "play-pause", NULL};
 static const char *prevcmd[] = { "playerctl", "previous", NULL};
 static const char *nextcmd[] = { "playerctl", "next", NULL};
-static const char *backup[]  = { "xbacklight", "-inc", "10", NULL};
-static const char *backdown[] = {"xbacklight", "-dec", "10", NULL};
+static const char *backup[]  = { "xbacklight", "-inc", "5", NULL};
+static const char *backdown[] = {"xbacklight", "-dec", "5", NULL};
 
 #include "shiftview.c"
 
@@ -83,6 +92,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_e,      togglebar,      {0} },
+    { MODKEY,                       XK_w,      tabmode,        {-1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -144,5 +154,6 @@ static Button buttons[] = {
     { ClkTagBar,            0,              Button5,        shiftview,      {.i = +1 } },
 	{ ClkTagBar,            MODKEY,         Button1,        combotag,       {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTabBar,            0,              Button1,        focuswin,       {0} },
 };
 
